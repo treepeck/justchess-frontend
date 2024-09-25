b5397e2fa30769949b8893c5ed9beeec8f835e59
Artem Belikov <artem.o.belikov@gmail.com>
2024-09-25 12:03:45 +0300

    feat: basic availible moves highlighting (king not implemented)

---
+++ src/components/timer/Timer.jsx
@@ -0,0 +1,11 @@
+// import styles from "./timer.module.css"
+
+// import {
+//   useState
+// } from "react"
+
+// export default function Timer({ control, bonus }) {
+
+//   const [time, setTime] = useState()
+
+// }
