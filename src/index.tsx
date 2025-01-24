7e13bb9c79b0f5156ed60c9f0f0af4ceab184d99
Artem Belikov <artem.o.belikov@gmail.com>
2025-01-24 13:17:35 +0200

    chore: manual webpack configuration insead of create-react-app

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,7 @@
-import ReactDOM from "react-dom/client"
 import "./index.css"
-
-import {
-  Route,
-  RouteObject,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom"
-import { AuthProvider } from "./context/Auth"
-import ConnProvider from "./context/Conn"
-import Home from "./pages/home/Home"
-import Play from "./pages/play/Play"
-
-const routes: RouteObject[] = createRoutesFromElements(
-  <Route element={<AuthProvider />}>
-    <Route element={<ConnProvider />}>
-      <Route path="/" element={<Home />} />
-      <Route path="/play/:id" element={<Play />} />
-    </Route>
-  </Route >
-)
-
-const router = createBrowserRouter(routes)
+import ReactDOM from "react-dom/client"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <RouterProvider router={router} />
+  <h1>Hello, World!</h1>
 )
---
+++ webpack.config.mjs
@@ -0,0 +1,37 @@
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+
+const prod = process.env.NODE_ENV === "production"
+
+export default {
+  mode: prod ? "production" : "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: import.meta.dirname + '/dist/',
+    filename: "[name][contenthash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"]
+        },
+        use: "ts-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css"
+    })
+  ],
+}
