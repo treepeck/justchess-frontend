0752c2cf20d32a3ff7cfff08aff584a40bb4e06d
Artem Bielikov <artem.bielikov@treepeck.com>
2025-03-12 11:00:00 +0200

    refactor: useReducer instead of the bunch of states

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,39 +1,119 @@
+// Styling.
 import "./Home.css"
+
+// Connection with server.
 import _WebSocket from "../../ws/ws"
-import { useEffect, useState } from "react"
+import { Message, MessageType } from "../../ws/message"
+
+// React stuff.
+import { useEffect, useReducer } from "react"
+
+// Contexts.
 import { useTheme } from "../../context/Theme"
 import { useNavigate } from "react-router-dom"
+import { useAuthentication } from "../../context/Authentication"
+
+// Components.
+import Table from "../../components/table/Table"
 import Dialog from "../../components/dialog/Dialog"
 import Slider from "../../components/slider/Slider"
 import Header from "../../components/header/Header"
-import { Message, MessageType } from "../../ws/message"
-import { useAuthentication } from "../../context/Authentication"
-import Table from "../../components/table/Table"
-import RadioButtons from "../../components/radio-buttons/RadioButtons"
 import Button from "../../components/button/Button"
 import { useEngineConf } from "../../context/EngineConf"
+import RadioButtons from "../../components/radio-buttons/RadioButtons"
 
-export type Room = {
+type Room = {
 	id: string,
-	b: number, // Time bonus.
 	c: number, // Time control.
+	b: number // Time bonus.
+}
+
+enum Action {
+	SET_SOCKET,
+	TOGGLE_DIALOG,
+	ADD_ROOM,
+	REMOVE_ROOM,
+	SET_OPPONENT,
+	SET_TIME_BONUS,
+	SET_TIME_CONTROL,
+	SET_CLIENTS_COUNTER,
+}
+
+type State = {
+	socket: _WebSocket | null,
+	isDialogActive: boolean,
+	rooms: Room[],
+	opponent: string,
+	timeBonus: number,
+	timeControl: number,
+	clientsCounter: number
+}
+
+const init: State = {
+	socket: null,
+	isDialogActive: false,
+	rooms: [],
+	opponent: "User",
+	timeBonus: 10,
+	timeControl: 10,
+	clientsCounter: 0,
+}
+
+interface IAction {
+	type: Action,
+	payload: any,
+}
+
+function reducer(state: State, action: IAction) {
+	switch (action.type) {
+		case Action.SET_SOCKET:
+			return { ...state, socket: action.payload }
+
+		case Action.TOGGLE_DIALOG:
+			return { ...state, isDialogActive: action.payload }
+
+		case Action.ADD_ROOM:
+			return {
+				...state,
+				// Do not add duplicates.
+				rooms: state.rooms.some(room => room.id === action.payload.id)
+					? state.rooms
+					: [...state.rooms, action.payload]
+			}
+
+		case Action.REMOVE_ROOM:
+			return {
+				...state,
+				rooms: state.rooms.filter(room => room.id !== action.payload)
+			}
+
+		case Action.SET_OPPONENT:
+			return { ...state, opponent: action.payload }
+
+		case Action.SET_TIME_CONTROL:
+			return { ...state, timeControl: action.payload }
+
+		case Action.SET_TIME_BONUS:
+			return { ...state, timeBonus: action.payload }
+
+		case Action.SET_CLIENTS_COUNTER:
+			return { ...state, clientsCounter: action.payload }
+	}
 }
 
 export default function Home() {
+	// Custom hooks.
 	const { theme } = useTheme()
 	const { user, accessToken } = useAuthentication()
 	const { threads, hashSize, difficulty, setThreads, setHashSize, setDifficulty } = useEngineConf()
 
+	// React-router hook.
 	const navigate = useNavigate()
-	const [socket, setSocket] = useState<_WebSocket | null>(null)
-	const [isDialogActive, setIsDialogActive] = useState<boolean>(false)
 
-	const [rooms, setRooms] = useState<Room[]>([])
-	const [opponent, setOpponent] = useState<string>("User")
-	const [timeBonus, setTimeBonus] = useState<number>(10)
-	const [timeControl, setTimeControl] = useState<number>(10)
-	const [clientsCounter, setClientsCounter] = useState<number>(0)
+	// Reducer.
+	const [state, dispatch] = useReducer(reducer, init)
 
+	// Connect to the Hub on page load.
 	useEffect(() => {
 		const s = new _WebSocket("/hub?", accessToken)
 
@@ -43,21 +123,18 @@ export default function Home() {
 			handleMessage(msg as Message)
 		}
 
-		s.socket.onclose = () => {
-			setSocket(null)
-		}
+		s.socket.onopen = () => dispatch({ type: Action.SET_SOCKET, payload: s })
 
-		s.socket.onopen = () => {
-			setSocket(s)
-		}
+		s.socket.onclose = () => dispatch({ type: Action.SET_SOCKET, payload: null })
 
 		return () => s.close()
 	}, [])
 
+	// Handle incomming messages from Hub.
 	function handleMessage(msg: Message) {
 		switch (msg.t) {
 			case MessageType.CLIENTS_COUNTER:
-				setClientsCounter(msg.d.c)
+				dispatch({ type: Action.SET_CLIENTS_COUNTER, payload: msg.d.c })
 				break
 
 			case MessageType.ADD_ROOM:
@@ -65,20 +142,11 @@ export default function Home() {
 					navigate(`/${user.id}`)
 					return
 				}
-
-				setRooms(prevRooms => {
-					// Do not add duplicates.
-					if (prevRooms.some(room => room.id === msg.d.id)) {
-						return prevRooms
-					}
-					return [...prevRooms, msg.d]
-				})
+				dispatch({ type: Action.ADD_ROOM, payload: msg.d })
 				break
 
 			case MessageType.REMOVE_ROOM:
-				setRooms(prevRooms => prevRooms.filter(room =>
-					room.id !== msg.d.id
-				))
+				dispatch({ type: Action.REMOVE_ROOM, payload: msg.d })
 				break
 		}
 	}
@@ -90,7 +158,7 @@ export default function Home() {
 			<Table
 				caption="Active games"
 				headerCols={["Creator", "Control", "Bonus"]}
-				bodyRows={rooms}
+				bodyRows={state.rooms}
 				bodyOnClick={(e) => {
 					const id = e.currentTarget.dataset.row
 					navigate(`/${id}`)
@@ -99,37 +167,39 @@ export default function Home() {
 
 			<Button
 				text="CREATE A NEW GAME"
-				onClick={() => setIsDialogActive(true)}
+				onClick={() => dispatch({ type: Action.TOGGLE_DIALOG, payload: true })}
 			/>
 
 			<div className="clients-counter">
-				Players in lobby: {clientsCounter}
+				Players in lobby: {state.clientsCounter}
 			</div>
 
-			{isDialogActive && (
-				<Dialog caption="Create a game" onClick={() => { setIsDialogActive(false) }}>
+			{state.isDialogActive && (
+				<Dialog caption="Create a game" onClick={() => dispatch({
+					type: Action.TOGGLE_DIALOG, payload: false
+				})}>
 					<>
 						<RadioButtons
-							caption="Choose an opponent: "
+							value={state.opponent}
 							options={["User", "Computer"]}
-							value={opponent}
-							setValue={setOpponent}
+							setValue={(val) => dispatch({ type: Action.SET_OPPONENT, payload: val })}
+							caption="Choose an opponent: "
 						/>
 						<Slider
-							value={timeControl}
-							setValue={setTimeControl}
+							value={state.timeControl}
+							setValue={(val) => dispatch({ type: Action.SET_TIME_CONTROL, payload: val })}
 							min={1}
 							max={180}
 							text="Time control in minutes: "
 						/>
 						<Slider
-							value={timeBonus}
-							setValue={setTimeBonus}
+							value={state.timeBonus}
+							setValue={(val) => dispatch({ type: Action.SET_TIME_BONUS, payload: val })}
 							min={0}
 							max={180}
 							text="Time bonus in seconds: "
 						/>
-						{opponent == "Computer" && (
+						{state.opponent == "Computer" && (
 							<div className="engine-params">
 								<p>Engine: Stockfish 16</p>
 								<Slider
@@ -157,7 +227,11 @@ export default function Home() {
 						<Button
 							text="Start"
 							onClick={() => {
-								socket!.sendCreateRoom(opponent == "Computer", timeControl, timeBonus)
+								state.socket?.sendCreateRoom(
+									state.opponent == "Computer",
+									state.timeControl,
+									state.timeBonus
+								)
 							}}
 						/>
 					</>
