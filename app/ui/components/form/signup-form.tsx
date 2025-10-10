fd6a5c44ab4bd1b249ae12d0d53e662757e94e0f
TimeSlayer <nikita.sorokin.personal@gmail.com>
2025-10-10 15:56:26 +0300

    fix: add pointer cursor to signup submit button

--- app/ui/components/form/signup-form.tsx
+++ app/ui/components/form/signup-form.tsx
@@ -53,7 +53,7 @@ export default function SignupForm() {
         <button
           disabled={pending}
           type="submit"
-          className="flex w-full justify-center rounded-md  bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 disabled:bg-indigo-800"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white cursor-pointer hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 disabled:bg-indigo-800"
         >
           Create account
         </button>
